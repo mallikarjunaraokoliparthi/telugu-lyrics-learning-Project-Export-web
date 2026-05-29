5e8e8b08a611a75f1036ab818d107d371576c8d3
Koliparti Mallikarjuna Rao <mallikarjunaraokoliparthi@gmail.com>
2026-05-29 10:47:19 +0530

    update..

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "284b562d5a019a20ee19ceee1918510a
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "5af301fd0a18b15238301f152e73427b",
+"flutter_bootstrap.js": "e7ea87dd347bc2496862edff64c3523b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
